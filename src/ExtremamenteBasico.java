d565d74302262fce292300f44e8ed452ecf90e3b
Dev_davilima <davinoronhalima210407@gmail.com>
2025-09-02 08:37:22 -0300

    Add files via upload

---
+++ src/ExtremamenteBasico.java
@@ -0,0 +1,11 @@
+public class ExtremamenteBasico {
+
+    public int n1;
+    public int n2;
+
+    public int soma;
+
+    public void soma(){
+        soma = n1+n2;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,21 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
-        int[] numeros;
-        numeros = new int[5];
+        ExtremamenteBasico numero = new ExtremamenteBasico();
 
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.println("Digite o " + (i + 1) + " valor ");
-            numeros[i] = sc.nextInt();
-        }
-        System.out.println("-----Resultado-----");
-        for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i] <= 0) {
-                System.out.println("X [" + i + "] =  " + numeros[i]);
+        numero.n1 = sc.nextInt();
+        numero.n2 = sc.nextInt();
+
+        numero.soma();
+
+        System.out.printf("X = %d%n", numero.soma);
+
             }
-        }
-    }
 }
